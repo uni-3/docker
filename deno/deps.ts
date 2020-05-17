805a6beb77a2f4795accbf23a0e2672346b47206
uni <7pluto8@gmail.com>
2020-05-18 08:32:38 +0900

    add deno container

---
+++ deno/deps.ts
@@ -0,0 +1,1 @@
+//export { test, runIfMain } from 'https://deno.land/std@v0.26.0/testing/mod.ts';
---
+++ deno/echo_server.ts
@@ -0,0 +1,9 @@
+// https://deno.land/std/examples/echo_server.ts
+const hostname = "0.0.0.0";
+const port = 8080;
+const listener = Deno.listen({ hostname, port });
+
+console.log(`Listening on ${hostname}:${port}`);
+for await (const conn of listener) {
+  Deno.copy(conn, conn);
+}
